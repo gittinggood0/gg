4f328549e0cc90a45241ce987bb8e8a63e9f4be3
faashraf <faashraf@iu.edu>
2021-04-26 05:20:43 -0500

    added questions

--- js/ch1.js
+++ js/ch1.js
@@ -1,1 +1,52 @@
 /* Chapter 1 Quiz */
+
+"use strict";
+
+// Questions
+let questions = [
+  {
+    question: "What is Version Control Software?",
+    answers: {
+      a: "Sends changes to the GitHub Cloud.",
+      b: "Stages Changes to be sent to the GitHub Cloud.",
+      c: "Retrieves changes from a newer version(s) of a file in the GitHub Cloud.",
+    },
+    correct: "b",
+  },
+  {
+    question: "T/F Web Development teams use GitHub to help enhance code run time.",
+    answers: {
+      a: "Sends changes to the GitHub Cloud.",
+      b: "Stages Changes to be sent to the GitHub Cloud.",
+      c: "Retrieves changes from a newer version(s) of a file in the GitHub Cloud.",
+    },
+    correct: "c",
+  },
+  {
+    question: "Which of the following is Git not useful for?",
+    answers: {
+      a: "Sends changes to the GitHub Cloud.",
+      b: "Stages Changes to be sent to the GitHub Cloud.",
+      c: "Retrieves changes from a newer version(s) of a file in the GitHub Cloud.",
+    },
+    correct: "a",
+  },
+  {
+    question: "T/F The relationship between the server you work on and your machine is bi-directional.",
+    answers: {
+      a: "Terminal/puTTY",
+      b: "Terminal/CyberDuck",
+      c: "winSCP/puTTY",
+    },
+    correct: "c",
+  },
+  {
+    question: "If I am on a Mac, I need to use ______ for accessing a server and ______ for file management on that same server.",
+    answers: {
+      a: "Terminal/puTTY",
+      b: "Terminal/CyberDuck",
+      c: "winSCP/puTTY",
+    },
+    correct: "b",
+  },
+];
